5c9d2af20c615ddecc97028695ba573f40bd0d54
Lindsey Trainor <ltrainor38@gmail.com>
2017-12-25 10:36:38 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def my_all?(collection)
-
+i = 0 
+return_values = []
+while i < collection.length 
+  return_values << yield(collection[i])
+end 
+if return_values.include?(false)
+  false 
+else 
+  true 
+end 
 end
+
+my_all?(collection) do |x|
+  
